b14742dc16fb52d0ed98238c25b2b218008e28b0
Juan Barbat <juan@barbat.dev>
2026-08-21 15:46:20 -0300

    fix(nix): pin valid 24.11 release inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Inert Home Manager boundary for cortex-dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
